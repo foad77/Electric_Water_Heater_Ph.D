b6d9377d55d71724e53ea2ecbadf60250ca1d781
foad <foad.najafi@wattmore.com>
2024-05-24 17:13:04 -1000

    removing the redundant files and results from the branch to make it lighter so, i can focus better on what needs to be changed

--- Input/K_mean/K_means.py
+++
@@ -1,9 +0,0 @@
-import imports
-current_day=365;lookback_length=365;
-execfile('Region_generator.py')
-min_slots=[1,2,3,4,5,6,10,12,15,20,30,60]
-cluster_set=[2,3,4,5,6,7,8,9,10]
-for min_slot in min_slots:
-    for n in cluster_set:
-        
-
